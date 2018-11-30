d02d6d250f9e73d5f0725c27d1c1baf0adacba24
Patrick Keane <pkeane92@yahoo.com>
2018-11-29 22:18:14 -0600

    about done with spotify-this-song function

--- liri.js
+++ liri.js
@@ -1,29 +1,82 @@
 require("dotenv").config();
 
-var keys = require('./keys.js')
+var keys = require('./keys.js');
 
-var Spotify = require('node-spotify-api') 
+var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify ({
+var appname = process.argv[2];
 
-    id: keys.spotify.id,
-    secret: keys.spotify.secret
-});
+var keyword = process.argv[3];
 
-spotify.search({type: 'track', query: 'All the Small Things'}, function(err, data) {
+if ((appname == "spotify-this-song") && (typeof keyword !== 'undefined')) {
 
-    if (err) {
+    var spotify = new Spotify ({
 
-        return console.log('Error occurred; ' + err);
-    }
+        id: keys.spotify.id,
+        secret: keys.spotify.secret
+    });
+   
+    console.log(keyword);
 
-    var songs = data.tracks.items;
-
-    for (i = 0; i < songs.length; i++) {
+    spotify.search({type: 'track', query: keyword}, function(err, data) {
+    
+        if (err) {
+    
+            return console.log('Error occurred; ' + err);
+        };
         
-        console.log(songs[i].artists[0].name);
-        console.log("-----------");
-        console.log(songs[i].preview_url);
+        var songs = data.tracks.items;
+    
+        for (i = 0; i < songs.length; i++) {
+            
+            console.log("Song name: " + songs[i].name);
+            console.log("Artist: " + songs[i].artists[0].name);
+            console.log("Album: " + songs[i].album.name);
+            console.log("Link to preview of the song (if avaialble): " + songs[i].preview_url);
+            console.log("-----------");
+    
+        }
+    })
 
-    }
-})
+}
+
+else if ((appname == "spotify-this-song") && (typeof keyword == 'undefined')) {
+
+    var spotify = new Spotify ({
+
+        id: keys.spotify.id,
+        secret: keys.spotify.secret
+    });
+    
+    spotify.search({type: 'track', query: 'The Sign' }, function(err, data) {
+    
+        if (err) {
+    
+            return console.log('Error occurred; ' + err);
+        };
+        
+        var songs = data.tracks.items;
+    
+            for (i = 0; i < songs.length; i++) {
+            
+                if ((songs[i].artists[0].name) == "Ace of Base") {
+                    
+                console.log("Song name: " + songs[i].name);
+                console.log("Artist: " + songs[i].artists[0].name);
+                console.log("Album: " + songs[i].album.name);
+                console.log("Link to preview of the song (if avaialble): " + songs[i].preview_url);
+                console.log("-----------");
+        
+            }
+
+        }
+
+        
+    })
+}
+
+else {
+
+    console.log("NAH BRO");
+}
+
